74626a8e127a068743531ecbdb38e2a23bf8a3ec
yanlei <yanlei0518@163.com>
2017-05-17 03:28:28 +0800

    身份验证

--- security/src/test/java/com/git/yanlei/security/AuthenticatorTests.java
+++ security/src/test/java/com/git/yanlei/security/AuthenticatorTests.java
@@ -1,17 +1,14 @@
 package com.git.yanlei.security;
 
-import java.util.Collections;
-
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
-import org.apache.shiro.subject.support.DelegatingSubject;
 import org.apache.shiro.util.Factory;
+import org.apache.shiro.util.ThreadContext;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,9 +19,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SecurityApplicationTests {
-    
-    private static final Logger log = LoggerFactory.getLogger(SecurityApplicationTests.class);
+public class AuthenticatorTests {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthenticatorTests.class);
 
     @Test
     public void contextLoads() {
@@ -44,56 +41,58 @@ public class SecurityApplicationTests {
         subject.logout();
     }
 
-    /**  
-     * @Description: <pre>multiRealm默认情况
+    /**
+     * @Description: <pre>
+     * multiRealm默认情况
      * securityManager.authenticator=ModularRealmAuthenticator
-     * securityManager.authenticator.authenticationStrategy=AtLeastOneSuccessfulStrategy</pre>
+     * securityManager.authenticator.authenticationStrategy=AtLeastOneSuccessfulStrategy
+     * </pre>
      * @author yanlei
      * @date 2017年5月13日 上午3:43:54
      * @version V1.0
-     */ 
+     */
     @Test
     public void testMultiRealm() {
         Subject subject = loginAs_ZhangSan("classpath:shiro-multi-realm.ini");
         Assert.assertTrue("is not authenticated", subject.isAuthenticated());
         subject.logout();
     }
-    
+
     @Test
     public void testJdbcRealm() {
         Subject subject = loginAs_ZhangSan("classpath:shiro-jdbc-realm.ini");
         Assert.assertTrue("is not authenticated", subject.isAuthenticated());
         subject.logout();
     }
-    
+
     @Test
     public void testAllSuccessfulRealm() {
         Subject subject = loginAs_ZhangSan("classpath:shiro-all-successful-strategy.ini");
         Assert.assertTrue("is not authenticated", subject.isAuthenticated());
         subject.logout();
     }
-    
+
     @Test
     public void testAtLeastOnSusscessfulRealm() {
         Subject subject = loginAs_ZhangSan("classpath:shiro-at-least-one-successful-strategy.ini");
         Assert.assertTrue("is not authenticated", subject.isAuthenticated());
         subject.logout();
     }
-    
+
     @Test
     public void testFirstSuccessfulRealm() {
         Subject subject = loginAs_ZhangSan("classpath:shiro-first-successful-strategy.ini");
         Assert.assertTrue("is not authenticated", subject.isAuthenticated());
         subject.logout();
     }
-    
+
     @Test
     public void testAtLeastTwoSuccessfulRealm() {
         Subject subject = loginAs_ZhangSan("classpath:shiro-at-least-two-successful-strategy.ini");
         Assert.assertTrue("is not authenticated", subject.isAuthenticated());
         subject.logout();
     }
-    
+
     @Test
     public void testOnlyOneSuccessfulRealm() {
         Subject subject = loginAs_ZhangSan("classpath:shiro-only-one-successful-strategy.ini");
@@ -117,6 +116,11 @@ public class SecurityApplicationTests {
         }
         log.info("{}登陆成功", username);
         return subject;
+    }
+    
+    @After
+    public void cleanSubject(){
+        ThreadContext.unbindSubject();
     }
 
 }
